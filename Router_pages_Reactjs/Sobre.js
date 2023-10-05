0e027d1f36beb7b958dd6c81dd153f4d86a34bd6
Felipecdc <124524102+Felipecdc@users.noreply.github.com>
2023-10-05 20:06:15 -0300

    Sobre.js

---
+++ Router_pages_Reactjs/Sobre.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+function Sobre() {
+  return (
+    <div className="App">
+      <h1>BEM VINDO A TELA DE SOBRE A EMPRESA</h1>
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
+
+export default Sobre;
