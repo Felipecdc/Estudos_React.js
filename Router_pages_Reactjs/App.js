775bbf74af77b9c0683cc9cbc0db718a44519ea7
Felipecdc <124524102+Felipecdc@users.noreply.github.com>
2023-10-05 20:04:26 -0300

    App.js

---
+++ Router_pages_Reactjs/App.js
@@ -0,0 +1,15 @@
+/*
+* Arquivo App onde está sendo chamado o arquivo de rotas entre as paginas...
+* uso do npm install react-router-dom
+*/
+
+import React from "react";
+import RoutesApp from "./Routes";
+
+function App() {
+  return (
+    <RoutesApp/>
+  );
+}
+
+export default App;
