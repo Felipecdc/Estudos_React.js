16977bcfa292046fdac2ca5a379f83573deb4d27
Felipecdc <124524102+Felipecdc@users.noreply.github.com>
2023-10-07 13:38:26 -0300

    App.js

---
+++ Filme_app_web/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import RoutesApp from "./Routes";
+
+function App() {
+  return (
+    <div className="App">
+      <ToastContainer autoClose={3000}/>
+      <RoutesApp/>
+    </div>
+  );
+}
+
+export default App;
