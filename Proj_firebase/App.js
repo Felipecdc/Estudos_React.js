890ed1e2820342e52dc8dc677249afbb78b886b6
Felipecdc <124524102+Felipecdc@users.noreply.github.com>
2023-10-10 20:25:35 -0300

    App.js

---
+++ Proj_firebase/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import RoutesApp from "./Routes";
+
+function App(){
+  return(
+    <BrowserRouter>
+      <RoutesApp/> // Arquivo Rotas
+    </BrowserRouter>
+  )
+}
+
+export default App;
